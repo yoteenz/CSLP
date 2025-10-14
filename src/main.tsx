a0e0e87063bbcb11b4fd525e83f44b6f28bb1992
yoteenz <kateenaarmstrong@gmail.com>
2025-10-14 13:05:51 -0500

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ComingSoonPage from './pages/ComingSoonPage.tsx'
+import './index.css'
+import './styles/coming-soon.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ComingSoonPage />
+  </React.StrictMode>,
+)
+
+
+
+
+
+
+
+
+
